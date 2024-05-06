6dd802b81b51b576c217c078a2b599afcf5f2ae1
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2024-05-06 23:07:37 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import './App.css'
+import {TodoProvidor} from "./contexts/index.js";
+import {useEffect, useState} from "react";
+
+function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id:Date.now(), ...todo}, ...prev]);
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos( (prev) => prev.map( (prevTodo) => (
+        prevTodo.id === id ? todo : prevTodo
+    ) ) )
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id != id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => {
+      prev.map((prevTodo) => {
+        prevTodo.id === id ? {...prevTodo, completed : !prevTodo.completed} : prevTodo
+      })
+    })
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos]);
+
+
+  return (
+    <TodoProvidor value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+
+    </TodoProvidor>
+  )
+}
+
+export default App
