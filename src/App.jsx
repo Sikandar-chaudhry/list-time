e91cfb3c4f97d077aa3f034d306a7de06ca76de5
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2024-05-09 23:35:02 +0500

    Creates TodoItem.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
-import {TodoProvidor} from "./contexts/index.js";
+import {TodoProvider} from "./contexts/index.js";
 import {useEffect, useState} from "react";
 import TodoForm from "./components/TodoForm.jsx";
+import {TodoItem} from "./components/index.js";
 
 function App() {
 
@@ -43,9 +44,27 @@ function App() {
 
 
   return (
-    <TodoProvidor value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
-      <TodoForm />
-    </TodoProvidor>
+      <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+        <div className="bg-green-100 min-h-screen py-8">
+          <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white border-2">
+            <h1 className="text-2xl font-bold text-center mb-8 mt-2 text-purple-700">Manage Your Todos</h1>
+            <div className="mb-4">
+
+              <TodoForm />
+            </div>
+            <div className="flex flex-wrap gap-y-3">
+              {/*Loop and Add TodoItem here */}
+              {todos.map((todo) => (
+                  <div key={todo.id}
+                       className='w-full'
+                  >
+                    <TodoItem todo={todo} />
+                  </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </TodoProvider>
   )
 }
 
