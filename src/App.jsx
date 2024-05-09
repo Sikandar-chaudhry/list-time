be946734430510aeacdf30c35c78ab73fb4146af
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2024-05-09 23:59:34 +0500

    Changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ function App() {
   }
 
   const toggleComplete = (id) => {
-    setTodos((prev) => {
-      prev.map((prevTodo) =>
-        prevTodo.id === id ? {...prevTodo, completed : !prevTodo.completed} : prevTodo
-      )
-    })
+    setTodos((prev) =>
+        prev.map((prevTodo) =>
+            prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo
+        )
+    )
   }
 
   useEffect(() => {
@@ -44,11 +44,11 @@ function App() {
 
   return (
       <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
-        <div className="bg-green-100 min-h-screen py-8">
+        <div className="bg-black min-h-screen py-8">
           <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white border-2">
-            <h1 className="text-2xl font-bold text-center mb-8 mt-2 text-purple-700">Manage Your Todos</h1>
+            <h1 className="text-2xl font-bold text-center mb-8 mt-2 text-white">Manage Your Todos</h1>
             <div className="mb-4">
-
+              {/* Todo form goes here */}
               <TodoForm />
             </div>
             <div className="flex flex-wrap gap-y-3">
